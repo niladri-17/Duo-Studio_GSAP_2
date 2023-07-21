b481c5134348dba1befe300d1e57d529f387ba03
Niladri Basak <niladri.basak.007@gmail.com>
2023-07-21 17:30:13 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,134 +1,150 @@
 function init() {
-    gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector(".main"),
-        smooth: true
-    });
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
 
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-    // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy(".main", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-    });
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    ScrollTrigger.refresh();
-
+  ScrollTrigger.refresh();
 }
 
-init()
+init();
 
-var crsr = document.querySelector(".cursor")
-var main = document.querySelector(".main")
+var crsr = document.querySelector(".cursor");
+var main = document.querySelector(".main");
 document.addEventListener("mousemove", function (dets) {
-    crsr.style.left = dets.x + 2 + "px"
-    crsr.style.top = dets.y + 14 + "px"
-})
+  crsr.style.left = dets.x + 2 + "px";
+  crsr.style.top = dets.y + 14 + "px";
+});
 
 gsap.from(".page1 h1,.page1 h2", {
-    y: 10,
-    rotate: 10,
-    opacity: 0,
-    delay: 0.3,
-    duration: 0.7
-})
+  y: 10,
+  rotate: 10,
+  opacity: 0,
+  delay: 0.3,
+  duration: 0.7,
+});
 
 var tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".page1 h1",
-        scroller: ".main",
-        // markers:true,
-        start: "top 27%",
-        end: "top 0",
-        scrub: 3
-    }
-})
-tl.to(".page1 h1", {
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    // markers:true,
+    start: "top 27%",
+    end: "top 0",
+    scrub: 3,
+  },
+});
+tl.to(
+  ".page1 h1",
+  {
     x: -100,
-}, "anim")
-tl.to(".page1 h2", {
-    x: 100
-}, "anim") // same variable anim is used so that these execute at the same time
-tl.to(".page1 video", {
-    width: "90%"
-}, "anim")
+  },
+  "anim"
+);
+tl.to(
+  ".page1 h2",
+  {
+    x: 100,
+  },
+  "anim"
+); // same variable anim is used so that these execute at the same time
+tl.to(
+  ".page1 video",
+  {
+    width: "90%",
+  },
+  "anim"
+);
 
 var tl2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".page1 h1",
-        scroller: ".main",
-        // markers:true,
-        start: "top -115%",
-        end: "top -120%",
-        scrub: 3
-    }
-})
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    // markers:true,
+    start: "top -115%",
+    end: "top -120%",
+    scrub: 3,
+  },
+});
 
 tl2.to(".main", {
-    backgroundColor: "#fff",
-})
+  backgroundColor: "#fff",
+});
 
 var tl3 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".page1 h1",
-        scroller: ".main",
-        // markers:true,
-        start: "top -280%",
-        end: "top -300%",
-        scrub: 3
-    }
-})
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    // markers:true,
+    start: "top -280%",
+    end: "top -300%",
+    scrub: 3,
+  },
+});
 
 tl3.to(".main", {
-    backgroundColor: "#0F0D0D"
-})
+  backgroundColor: "#0F0D0D",
+});
 
-
-var boxes = document.querySelectorAll(".box")
+var boxes = document.querySelectorAll(".box");
 boxes.forEach(function (elem) {
-    elem.addEventListener("mouseenter", function () {
-        var att = elem.getAttribute("data-image")
-        crsr.style.width = "470px"
-        crsr.style.height = "370px"
-        crsr.style.borderRadius = "0"
-        crsr.style.backgroundImage = `url(${att})`
-    })
-    elem.addEventListener("mouseleave", function () {
-        elem.style.backgroundColor = "transparent"
-        crsr.style.width = "20px"
-        crsr.style.height = "20px"
-        crsr.style.borderRadius = "50%"
-        crsr.style.backgroundImage = `none`
-    })
-})
+  elem.addEventListener("mouseenter", function () {
+    var att = elem.getAttribute("data-image");
+    crsr.style.width = "470px";
+    crsr.style.height = "370px";
+    crsr.style.borderRadius = "0";
+    crsr.style.backgroundImage = `url(${att})`;
+  });
+  elem.addEventListener("mouseleave", function () {
+    elem.style.backgroundColor = "transparent";
+    crsr.style.width = "20px";
+    crsr.style.height = "20px";
+    crsr.style.borderRadius = "50%";
+    crsr.style.backgroundImage = `none`;
+  });
+});
 
-
-
-var navWork = document.querySelectorAll(".work")
-var work = document.querySelector(".purple")
+var navWork = document.querySelectorAll(".work");
+var work = document.querySelector(".purple");
 navWork.forEach(function (elem) {
-    elem.addEventListener("mouseenter", function () {
-        work.style.display = "block"
-        work.style.opacity = "1"
-    })
-    elem.addEventListener("mouseleave", function () {
-        work.style.display = "none"
-        work.style.opacity = "0"
-    })
-})
+  elem.addEventListener("mouseenter", function () {
+    work.style.display = "block";
+    work.style.opacity = "1";
+  });
+  elem.addEventListener("mouseleave", function () {
+    work.style.display = "none";
+    work.style.opacity = "0";
+  });
+});
 
 // var navStudio = document.querySelectorAll(".studio")
 // var studio = document.querySelector("#studio")
@@ -147,59 +163,103 @@ const workboxes = gsap.utils.toArray(".box-work");
 const loop = horizontalLoop(workboxes, { paused: false, repeat: -1 });
 
 function horizontalLoop(items, config) {
-    items = gsap.utils.toArray(items);
-    config = config || {};
-    let tl = gsap.timeline({ repeat: config.repeat, paused: config.paused, defaults: { ease: "none" }, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100) }),
-        length = items.length,
-        startX = items[0].offsetLeft,
-        times = [],
-        widths = [],
-        xPercents = [],
-        curIndex = 0,
-        pixelsPerSecond = (config.speed || 1) * 100,
-        snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
-        totalWidth, curX, distanceToStart, distanceToLoop, item, i;
-    gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-        xPercent: (i, el) => {
-            let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
-            xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
-            return xPercents[i];
-        }
-    });
-    gsap.set(items, { x: 0 });
-    totalWidth = items[length - 1].offsetLeft + xPercents[length - 1] / 100 * widths[length - 1] - startX + items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX") + (parseFloat(config.paddingRight) || 0);
-    for (i = 0; i < length; i++) {
-        item = items[i];
-        curX = xPercents[i] / 100 * widths[i];
-        distanceToStart = item.offsetLeft + curX - startX;
-        distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
-        tl.to(item, { xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond }, 0)
-            .fromTo(item, { xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100) }, { xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false }, distanceToLoop / pixelsPerSecond)
-            .add("label" + i, distanceToStart / pixelsPerSecond);
-        times[i] = distanceToStart / pixelsPerSecond;
+  items = gsap.utils.toArray(items);
+  config = config || {};
+  let tl = gsap.timeline({
+      repeat: config.repeat,
+      paused: config.paused,
+      defaults: { ease: "none" },
+      onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100),
+    }),
+    length = items.length,
+    startX = items[0].offsetLeft,
+    times = [],
+    widths = [],
+    xPercents = [],
+    curIndex = 0,
+    pixelsPerSecond = (config.speed || 1) * 100,
+    snap = config.snap === false ? (v) => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+    totalWidth,
+    curX,
+    distanceToStart,
+    distanceToLoop,
+    item,
+    i;
+  gsap.set(items, {
+    // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+    xPercent: (i, el) => {
+      let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+      xPercents[i] = snap(
+        (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
+          gsap.getProperty(el, "xPercent")
+      );
+      return xPercents[i];
+    },
+  });
+  gsap.set(items, { x: 0 });
+  totalWidth =
+    items[length - 1].offsetLeft +
+    (xPercents[length - 1] / 100) * widths[length - 1] -
+    startX +
+    items[length - 1].offsetWidth *
+      gsap.getProperty(items[length - 1], "scaleX") +
+    (parseFloat(config.paddingRight) || 0);
+  for (i = 0; i < length; i++) {
+    item = items[i];
+    curX = (xPercents[i] / 100) * widths[i];
+    distanceToStart = item.offsetLeft + curX - startX;
+    distanceToLoop =
+      distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+    tl.to(
+      item,
+      {
+        xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
+        duration: distanceToLoop / pixelsPerSecond,
+      },
+      0
+    )
+      .fromTo(
+        item,
+        {
+          xPercent: snap(
+            ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
+          ),
+        },
+        {
+          xPercent: xPercents[i],
+          duration:
+            (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+          immediateRender: false,
+        },
+        distanceToLoop / pixelsPerSecond
+      )
+      .add("label" + i, distanceToStart / pixelsPerSecond);
+    times[i] = distanceToStart / pixelsPerSecond;
+  }
+  function toIndex(index, vars) {
+    vars = vars || {};
+    Math.abs(index - curIndex) > length / 2 &&
+      (index += index > curIndex ? -length : length); // always go in the shortest direction
+    let newIndex = gsap.utils.wrap(0, length, index),
+      time = times[newIndex];
+    if (time > tl.time() !== index > curIndex) {
+      // if we're wrapping the timeline's playhead, make the proper adjustments
+      vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
+      time += tl.duration() * (index > curIndex ? 1 : -1);
     }
-    function toIndex(index, vars) {
-        vars = vars || {};
-        (Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
-        let newIndex = gsap.utils.wrap(0, length, index),
-            time = times[newIndex];
-        if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
-            vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
-            time += tl.duration() * (index > curIndex ? 1 : -1);
-        }
-        curIndex = newIndex;
-        vars.overwrite = true;
-        return tl.tweenTo(time, vars);
-    }
-    tl.next = vars => toIndex(curIndex + 1, vars);
-    tl.previous = vars => toIndex(curIndex - 1, vars);
-    tl.current = () => curIndex;
-    tl.toIndex = (index, vars) => toIndex(index, vars);
-    tl.times = times;
-    tl.progress(1, true).progress(0, true); // pre-render for performance
-    if (config.reversed) {
-        tl.vars.onReverseComplete();
-        tl.reverse();
-    }
-    return tl;
+    curIndex = newIndex;
+    vars.overwrite = true;
+    return tl.tweenTo(time, vars);
+  }
+  tl.next = (vars) => toIndex(curIndex + 1, vars);
+  tl.previous = (vars) => toIndex(curIndex - 1, vars);
+  tl.current = () => curIndex;
+  tl.toIndex = (index, vars) => toIndex(index, vars);
+  tl.times = times;
+  tl.progress(1, true).progress(0, true); // pre-render for performance
+  if (config.reversed) {
+    tl.vars.onReverseComplete();
+    tl.reverse();
+  }
+  return tl;
 }
